d988c855cd8fb8efd9a1ed0f8ed7e636db342853
Raquel Brito <raquelbrito662@gmail.com>
2024-05-26 14:07:34 -0300

    fixes

--- lib/language_detector/language_detector.rb
+++ lib/language_detector/language_detector.rb
@@ -22,7 +22,7 @@ module LanguageDetector
       detector = CLD3::NNetLanguageIdentifier.new(min_num_bytes: 0, max_num_bytes: 1000)
       result = detector.find_language(cleaned_text)
 
-      result&.language || "unknown"
+      result&.language.upcase || "unknown"
     end
   end
 end
--- spec/language_detector_spec.rb
+++ spec/language_detector_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe LanguageDetector do
   it "does something useful" do
     expect(false).to eq(true)
   end
+
+  it 'detects English text correctly' do
+    detector = LanguageDetector::Detector.new("<p>Hello, this is a test!</p>")
+    expect(detector.detect_language).to eq('en')
+  end
+
+  it 'detects French text correctly' do
+    detector = LanguageDetector::Detector.new("<p>Bonjour, ceci est un test !</p>")
+    expect(detector.detect_language).to eq('fr')
+  end
+
+  it 'removes HTML tags and punctuation' do
+    detector = LanguageDetector::Detector.new("<p>Hello, this is a test!</p>")
+    cleaned_text = detector.clean_text
+    expect(cleaned_text).to eq('Hello this is a test')
+  ends
 end
